fc520a8619947abae77db951b9265c9135d6621b
AnaflaviaDiaz <anaflaviadiazmartel5a@gmail.com>
2026-05-21 18:37:04 +0200

    feat: basic java

--- src/FormatStrings.java
+++ src/FormatStrings.java
@@ -10,5 +10,30 @@ public class FormatStrings {
 
         //  Metodo printf
         System.out.printf("Nombre: %s, Edad: %d, Salario: $%.2f", name, age, salary);
+
+        var employerNumber = 12;
+        // text block
+        // \s ==> space, \t ==> tabulador
+        // $%.2f ==> para 2 decimales
+        // %04d ==> 4 digitos, se completa con 0s adelante
+        msg = """
+                %nDetalle Persona: \s
+                -----
+                \tNombre: %s
+                \tNo. Empleado: %04d
+                \tEdad: %d años
+                \tSalario: $%.2f
+                """.formatted(name, employerNumber, age, salary);
+        System.out.println(msg);
+
+        // formateo con textBlock y printf
+        System.out.printf("""
+                %nDetalle Persona: \s
+                -----
+                \tNombre: %s
+                \tNo. Empleado: %04d
+                \tEdad: %d años
+                \tSalario: $%.2f
+                """, name, employerNumber, age, salary);
     }
 }
---
+++ src/IdGenerator.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class IdGenerator {
+    static void main() {
+        var random = new Random();
+        var console = new Scanner(System.in);
+
+        System.out.println("***UNIQUE ID GENERATOR SYSTEM***");
+
+        System.out.println("What's your name? ");
+        var name = console.nextLine();
+
+        System.out.println("What's your lastname? ");
+        var lastname = console.nextLine();
+
+        System.out.println("When is your birth year? ");
+        var birthday = console.nextLine();
+
+        var randomNumber = random.nextInt(9999)+1;
+        var result = name.substring(0, 2)
+                + lastname.substring(0, 2)
+                + birthday.substring(birthday.length()-2)
+                + String.format("%04d", randomNumber);
+
+        System.out.printf("""
+                \tYour ID is:
+                \t%s
+                \tCongrats!
+                """, result.toUpperCase());
+    }
+}

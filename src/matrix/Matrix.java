2213734d4a7c40a000a0d4a698fb0884708b8705
AnaflaviaDiaz <anaflaviadiazmartel5a@gmail.com>
2026-06-09 11:32:13 +0200

    feat: add matrix examples

---
+++ src/matrix/Matrix.java
@@ -0,0 +1,20 @@
+package matrix;
+
+public class Matrix {
+    // matriz de 3 columnas y 2 renglones
+    static void main() {
+        // inicializando matriz
+        var matrix = new int[][]{
+                {100, 200, 300},
+                {400, 500, 600}
+        };
+
+        // 1. ciclo externo - filas
+        for (int i = 0; i < matrix.length; i++) {
+            // 2. ciclo interno - columnas
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.println("matrix["+i+"]["+j+ "]" + matrix[i][j]);
+            }
+        }
+    }
+}

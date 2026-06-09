96eadbe0c741378d126dea95029b788a8e1a02c0
AnaflaviaDiaz <anaflaviadiazmartel5a@gmail.com>
2026-06-09 14:51:21 +0200

    feat: functions

---
+++ src/functions/Even.java
@@ -0,0 +1,16 @@
+package functions;
+
+import java.util.Scanner;
+
+public class Even {
+    static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    static void main() {
+        System.out.println("Ingresa un número");
+        var console = new Scanner(System.in);
+        var num = Integer.parseInt(console.nextLine());
+        System.out.println(isEven(num) ? "Es par" :  "Es impar");
+    }
+}
---
+++ src/functions/Greetings.java
@@ -0,0 +1,13 @@
+package functions;
+
+public class Greetings {
+
+    static void greeting(String message) {
+        System.out.println("Mensaje: " + message);
+    }
+
+    static void main() {
+        greeting("Hola");
+        greeting("Bye");
+    }
+}

08c89856a5efb52a5a96cb32f14cad5f1bf9d7dd
AnaflaviaDiaz <anaflaviadiazmartel5a@gmail.com>
2026-06-11 12:45:06 +0200

    feat: generate unique ID

--- src/classes/person/Person.java
+++ src/classes/person/Person.java
@@ -1,12 +1,28 @@
 package classes.person;
 
 public class Person {
+    public static int countPeople = 0;
+    private final int id;
     private String name;
     private String lastname;
 
     public Person(String name, String lastname) {
         this.name = name;
         this.lastname = lastname;
+
+        // incremento del atributo count
+        this.id = ++Person.countPeople;
+    }
+
+    // sobreescribir metodo toString de la Clase Object
+    // que implitamente todas las clases extienden de esta
+    @Override
+    public String toString() {
+        return "ID: " + this.id + ", Nombre: " + this.name + ", Apellido: " + this.lastname;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -23,10 +39,5 @@ public class Person {
 
     public void setLastname(String lastname) {
         this.lastname = lastname;
-    }
-
-    public void showPerson() {
-        System.out.println("Nombre: " + this.name);
-        System.out.println("Nombre: " + this.lastname + "\n");
     }
 }
--- src/classes/person/test/PersonTest.java
+++ src/classes/person/test/PersonTest.java
@@ -4,10 +4,15 @@ import classes.person.Person;
 public class PersonTest {
     static void main() {
         System.out.println("*** Creacion de Clase y Objetos Persona ***");
+        // System.out.println("Variable estática: " + Person.countPeople);
         var obj1 =  new Person("Ana", "Diaz");
-        obj1.showPerson();
+        // System.out.println(obj1.toString());
+        System.out.println(obj1); // automaticamente, se llama a toString
+        System.out.println("Variable estática: " + Person.countPeople);
 
-        var obj2 =  new Person("Maria", "Paredes");
-        obj2.showPerson();
+        // segundo objeto
+        var obj2 =  new Person("Laila", "Fernandez");
+        System.out.println(obj2);
+        System.out.println("Variable estática: " + Person.countPeople);
     }
 }
